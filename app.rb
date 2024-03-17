61f14eab36c144184d635f2e18522444b6fd5f02
Aleksei Borovikov <161416353+aborov@users.noreply.github.com>
2024-03-17 23:21:59 +0000

    initial commit

--- app.rb
+++ app.rb
@@ -2,8 +2,21 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:square_new)
+end
+
+get("/square/new") do
+  erb(:square_new)
+end
+
+get("/square_root/new") do
+  erb(:square_root_new)
+end
+
+get("/random/new") do
+  erb(:randon_new)
+end
+
+get("/payment/new") do
+  erb(:payment_new)
 end
